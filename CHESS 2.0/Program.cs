75419137ace3bc5e1b7bc95ce598d47b281b4b52
runar.oosterheert <runar.oosterheert@1H85192L3Z>
2026-04-10 10:50:20 +0200

    Add project files.

---
+++ CHESS 2.0/Program.cs
@@ -0,0 +1,85 @@
+﻿
+using System.Security.AccessControl;
+
+string ANSI(Piece Text, int color) // Text Color    (Credit to Harry, he helped with ANSI)
+{
+    Piece text = Text;
+    string ansi = $"\u001b[38;2;{(color >> 16) & 0xff};{(color >> 8) & 0xff};{color & 0xff}m";
+    return ansi + text;
+}
+
+// Places the pieces in the starting setup
+Piece[,] board = new Piece[,] { 
+                        { Piece.Rook, Piece.Knight, Piece.Bishop, Piece.King, Piece.Queen, Piece.Bishop, Piece.Knight, Piece.Rook },
+                        { Piece.Pawn, Piece.Pawn, Piece.Pawn, Piece.Pawn, Piece.Pawn, Piece.Pawn, Piece.Pawn, Piece.Pawn },
+                        { Piece.____, Piece.____, Piece.____, Piece.____, Piece.____, Piece.____, Piece.____, Piece.____ },
+                        { Piece.____, Piece.____, Piece.____, Piece.____, Piece.____, Piece.____, Piece.____, Piece.____ },
+                        { Piece.____, Piece.____, Piece.____, Piece.____, Piece.____, Piece.____, Piece.____, Piece.____ },
+                        { Piece.____, Piece.____, Piece.____, Piece.____, Piece.____, Piece.____, Piece.____, Piece.____ },
+                        { Piece.Pawn, Piece.Pawn, Piece.Pawn, Piece.Pawn, Piece.Pawn, Piece.Pawn, Piece.Pawn, Piece.Pawn },
+                        { Piece.Rook, Piece.Knight, Piece.Bishop, Piece.King, Piece.Queen, Piece.Bishop, Piece.Knight, Piece.Rook }};
+
+// Keeps track of the position of the selected square
+int selectedRow = 3;
+int selectedColumn = 3;
+string WhSq(int Row, int Col) // White square
+{
+    Console.BackgroundColor = ConsoleColor.White;
+    bool isSelected = (Row == selectedRow && Col == selectedColumn);
+    string whiteSquare = ANSI(board[Row, Col], isSelected ? 0x85FFB2 : 0xFFFFFF);
+    Console.ResetColor();
+    return whiteSquare;
+}
+string BlSq(int Row, int Col) // Black square
+{
+    bool isSelected = (Row == selectedRow && Col == selectedColumn);
+    string blackSquare = ANSI(board[Row, Col], isSelected? 0x85FFB2 : 0x3B3B3B);
+    return blackSquare;
+}
+
+// Start of game loop
+while (true)
+{
+    Console.Clear();
+    for (int length = 0; length < 8;  length++) // Draws the board
+    {
+        for (int height = 0; height < 8; height++)
+        {
+            Console.SetCursorPosition(3 + length * 8, 2 + height * 3);
+            if ((length + height) % 2 == 0)
+            {
+                Console.Write($"{WhSq(height, length)}\t");
+            }
+            else
+            {
+                Console.Write($"{BlSq(height, length)}\t");
+            }
+        }
+    }
+
+    // Move the selected square using arrow keys
+    Console.WriteLine(selectedRow + "," + selectedColumn);
+    ConsoleKey Navigate = Console.ReadKey(true).Key;
+    switch (Navigate)
+    {
+        case ConsoleKey.UpArrow:
+            selectedRow--;
+            break;
+
+        case ConsoleKey.DownArrow:
+            selectedRow++;
+            break;
+
+        case ConsoleKey.LeftArrow:
+            selectedColumn--;
+            break;
+
+        case ConsoleKey.RightArrow:
+            selectedColumn++;
+            break;
+    }
+}
+enum Piece
+{
+    Rook, Knight, Bishop, King, Queen, Pawn, ____
+}
