55f6f8ce980c625312c19c369ab0b5dbc47f6b42
runar.oosterheert <runar.oosterheert@1H85192L3Z>
2026-04-17 15:28:18 +0200

    Fixed an issue where you had to press enter twice for it to react fixed an issue where the text of anything the green square went over became gray Got a new issue where sometimes when updating the entire board the screen (board excluded) becomes green/yellow

--- CHESS 2.0/Program.cs
+++ CHESS 2.0/Program.cs
@@ -1,15 +1,14 @@
-﻿using System.Security.AccessControl;
-
+﻿
 // Places the pieces in the starting setup
 Piece[,] board = new Piece[,] { 
-                        { Piece.Rook, Piece.Knight, Piece.Bishop, Piece.King, Piece.Queen, Piece.Bishop, Piece.Knight, Piece.Rook },
-                        { Piece.Pawn___, Piece.Pawn___, Piece.Pawn___, Piece.Pawn___, Piece.Pawn___, Piece.Pawn___, Piece.Pawn___, Piece.Pawn___ },
+                        { Piece._Rook__, Piece.Knight_, Piece.Bishop_, Piece._King__, Piece._Queen_, Piece.Bishop_, Piece.Knight_, Piece._Rook__ },
+                        { Piece._Pawn__, Piece._Pawn__, Piece._Pawn__, Piece._Pawn__, Piece._Pawn__, Piece._Pawn__, Piece._Pawn__, Piece._Pawn__ },
                         { Piece._______, Piece._______, Piece._______, Piece._______, Piece._______, Piece._______, Piece._______, Piece._______ },
                         { Piece._______, Piece._______, Piece._______, Piece._______, Piece._______, Piece._______, Piece._______, Piece._______ },
                         { Piece._______, Piece._______, Piece._______, Piece._______, Piece._______, Piece._______, Piece._______, Piece._______ },
                         { Piece._______, Piece._______, Piece._______, Piece._______, Piece._______, Piece._______, Piece._______, Piece._______ },
-                        { Piece.Pawni__, Piece.Pawni__, Piece.Pawni__, Piece.Pawni__, Piece.Pawni__, Piece.Pawni__, Piece.Pawni__, Piece.Pawni__ },
-                        { Piece.Rooki__, Piece.Knighti, Piece.Bishopi, Piece.Kingi__, Piece.Queeni_, Piece.Bishopi, Piece.Knighti, Piece.Rooki__ }};
+                        { Piece._Pawni_, Piece._Pawni_, Piece._Pawni_, Piece._Pawni_, Piece._Pawni_, Piece._Pawni_, Piece._Pawni_, Piece._Pawni_ },
+                        { Piece._Rooki_, Piece.Knighti, Piece.Bishopi, Piece._Kingi_, Piece.Queeni_, Piece.Bishopi, Piece.Knighti, Piece._Rooki_ }};
 
 // Keeps track of the position of the selected square
 int selectedRow = 3;
@@ -30,7 +29,25 @@ void DrawSquare(int Row, int Col, bool canBeCaptured, bool canBeMovedTo) // Draw
         canBeCaptured? ConsoleColor.Red : 
         canBeMovedTo? ConsoleColor.DarkYellow : 
         isWhite ? ConsoleColor.White : ConsoleColor.DarkGray;
-    
+
+    if (board[Row, Col] == Piece._Rooki_ ||     // Checks if piece belongs to White, changes its color accordingly
+                board[Row, Col] == Piece.Knighti ||
+                board[Row, Col] == Piece.Bishopi ||
+                board[Row, Col] == Piece.Queeni_ ||
+                board[Row, Col] == Piece._Kingi_ ||
+                board[Row, Col] == Piece._Pawni_)
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+    }
+    else if (board[Row, Col] == Piece._______)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+    }
+
     // Draws the whole square
     if (Row == 0)
     {
@@ -48,7 +65,6 @@ void DrawSquare(int Row, int Col, bool canBeCaptured, bool canBeMovedTo) // Draw
     Console.SetCursorPosition(Col * 7, 1 + Row * 3);
     Console.Write("       ");
     Console.SetCursorPosition(Col * 7, Row * 3);
-    Console.ResetColor();
 }
 
 void PawnMove(int tempSelectedRow, int tempSelectedColumn)
@@ -56,11 +72,11 @@ void PawnMove(int tempSelectedRow, int tempSelectedColumn)
     Piece position = board[tempSelectedRow, tempSelectedColumn];
     bool isWhite = true;
 
-    if (position == Piece.Pawni__) // Determines which side the pawn belongs to
+    if (position == Piece._Pawni_) // Determines which side the pawn belongs to
     {
         isWhite = true;
     }
-    else if (position == Piece.Pawn___)
+    else if (position == Piece._Pawn__)
     {
         isWhite = false;
     }
@@ -69,19 +85,22 @@ void PawnMove(int tempSelectedRow, int tempSelectedColumn)
     {
         if (isWhite)
         {
-            DrawSquare(tempSelectedRow - 1, tempSelectedColumn, false, true);
-            if (tempSelectedRow == 6)
+            if (board[tempSelectedRow - 1, tempSelectedColumn] == Piece._______) // Shows the squares the pawn can move to, if any
             {
-                DrawSquare(tempSelectedRow - 2, tempSelectedColumn, false, true);
+                DrawSquare(tempSelectedRow - 1, tempSelectedColumn, false, true);
+                if (tempSelectedRow == 6 && board[tempSelectedRow - 2, tempSelectedColumn] == Piece._______)
+                {
+                    DrawSquare(tempSelectedRow - 2, tempSelectedColumn, false, true);
+                }
             }
-        }
-        MoveSelectedSquare();
-        if (MoveSelectedSquare() == ConsoleKey.Enter)
-        {
-            if (board[selectedRow, selectedColumn] == Piece._______)
+            else
+            {
+                break;
+            }
+            if (MoveSelectedSquare() == ConsoleKey.Enter && board[selectedRow, selectedColumn] == Piece._______) // Moves the pawn if possible
             {
                 board[tempSelectedRow, tempSelectedColumn] = Piece._______;
-                board[selectedRow, selectedColumn] = Piece.Pawni__;
+                board[selectedRow, selectedColumn] = Piece._Pawni_;
                 DrawBoard();
                 break;
             }
@@ -97,67 +116,55 @@ void DrawBoard()
         for (int height = 0; height < 8; height++)
         {
             Console.SetCursorPosition(3 + length * 10, 2 + height * 3);
-            if (board[height, length] == Piece.Rooki__ ||     // Checks if piece belongs to White, changes its color accordingly
-                board[height, length] == Piece.Knighti ||
-                board[height, length] == Piece.Bishopi ||
-                board[height, length] == Piece.Queeni_ ||
-                board[height, length] == Piece.Kingi__ ||
-                board[height, length] == Piece.Pawni__)
-            {
-                Console.ForegroundColor = ConsoleColor.Cyan;
-            }
-            else if (board[height, length] == Piece._______)
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.DarkMagenta;
-            }
+            
             DrawSquare(height, length, false, false);
         }
     }
 }
 DrawBoard();
-ConsoleKey MoveSelectedSquare()
+void UpdateSquare(int offsetRow, int offsetCol)
+{
+    DrawSquare(selectedRow + offsetRow, selectedColumn + offsetCol, false, false);
+}
+ConsoleKey MoveSelectedSquare() // Navigate the board
 {
     ConsoleKey Navigate = Console.ReadKey(true).Key;
     switch (Navigate)
     {
         case ConsoleKey.UpArrow:
             selectedRow--;
-            DrawSquare(selectedRow + 1, selectedColumn, false, false);
-            DrawSquare(selectedRow, selectedColumn, false, false);
+            UpdateSquare(1, 0);
+            UpdateSquare(0, 0);
             break;
 
         case ConsoleKey.DownArrow:
             selectedRow++;
-            DrawSquare(selectedRow - 1, selectedColumn, false, false);
-            DrawSquare(selectedRow, selectedColumn, false, false);
+            UpdateSquare(-1, 0);
+            UpdateSquare(0, 0);
             break;
 
         case ConsoleKey.LeftArrow:
             selectedColumn--;
-            DrawSquare(selectedRow, selectedColumn + 1, false, false);
-            DrawSquare(selectedRow, selectedColumn, false, false);
+            UpdateSquare(0, 1);
+            UpdateSquare(0, 0);
             break;
 
         case ConsoleKey.RightArrow:
             selectedColumn++;
-            DrawSquare(selectedRow, selectedColumn - 1, false, false);
-            DrawSquare(selectedRow, selectedColumn, false, false);
+            UpdateSquare(0, -1);
+            UpdateSquare(0, 0);
             break;
     }
     return Navigate;
 }
+
 // Start of game loop
 while (true)
 {
-    MoveSelectedSquare();
     if (MoveSelectedSquare() == ConsoleKey.Enter)
     {
-        if (board[selectedRow, selectedColumn] == Piece.Pawni__ ||
-            board[selectedRow, selectedColumn] == Piece.Pawn___)
+        if (board[selectedRow, selectedColumn] == Piece._Pawni_ ||
+            board[selectedRow, selectedColumn] == Piece._Pawn__)
         {
             PawnMove(selectedRow, selectedColumn);
         }
@@ -165,5 +172,5 @@ while (true)
 }
 enum Piece // List of possible pieces
 {
-    Rooki__, Rook, Knighti, Knight, Bishopi, Bishop, Kingi__, King, Queeni_, Queen, Pawni__, Pawn___, _______
+    _Rooki_, _Rook__, Knighti, Knight_, Bishopi, Bishop_, _Kingi_, _King__, Queeni_, _Queen_, _Pawni_, _Pawn__, _______
 }
